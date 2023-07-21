d74f05753470412feef503067ac4d7924660926e
Vishal Trivedi <vishal.trivedi5.2002@gmail.com>
2023-07-21 16:10:03 -0400

    z-index to fix button not working

--- src/Pages/NameAndButton.js
+++ src/Pages/NameAndButton.js
@@ -2,7 +2,7 @@ import React from "react";
 import '../Component/Name.css';
 import "../Component/DownloadBTN.css";
 
-const NameAndButton = () => {
+function NameAndButton() {
   const scrollToSection = (event, sectionId) => {
     event.preventDefault();
     const section = document.getElementById(sectionId);
@@ -13,6 +13,7 @@ const NameAndButton = () => {
     <div className="flex flex-col items-center justify-center min-h-screen bg-gray-100 container">
       {/* Name Section */}
       <div>
+        <link href="" rel="stylesheet" />
         <svg viewBox="0 0 1320 300" className="name-svg">
           <text x="50%" y="50%" dy=".35em" textAnchor="middle" className="text">
             Vishal Trivedi
@@ -26,10 +27,12 @@ const NameAndButton = () => {
       {/* Button Section */}
       <div className="button mt-8 sm:mt-12 md:mt-16 lg:mt-20 xl:mt-24">
         <div
-          className="relative w-40 h-40 sm:w-32 sm:h-32 md:w-40 md:h-40 lg:w-44 lg:h-44 xl:w-52 xl:h-52"
-          onClick={(e) => scrollToSection(e, "downloads")}
-        >
-          <label htmlFor="button" className="block w-full h-full">
+          className="relative w-40 h-40 sm:w-32 sm:h-32 md:w-40 md:h-40 lg:w-44 lg:h-44 xl:w-52 xl:h-52">
+          <label 
+            htmlFor="button" 
+            onClick={(e) => scrollToSection(e, "downloads")}
+            className="block w-full h-full"
+          >
             <div className="absolute inset-0 flex items-center justify-center transition duration-300 bg-opacity-0 rounded-lg hover:bg-opacity-10 button_inner q">
               <i className="l ion-log-in"></i>
               <span className="text-xs sm:text-sm md:text-base lg:text-lg xl:text-xl">
