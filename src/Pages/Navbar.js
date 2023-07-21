abd3f24c4972a228d45890ae350718ac521491e1
Vishal Trivedi <vishal.trivedi5.2002@gmail.com>
2023-07-21 11:56:15 -0400

    Close navbar after clicking a link

--- src/Pages/Navbar.js
+++ src/Pages/Navbar.js
@@ -1,9 +1,11 @@
-import React from "react";
+import React, { useState } from "react";
 import { Navbar, Nav, OverlayTrigger, Tooltip } from "react-bootstrap";
 import LogoImage from "../Assets/gh.png"; // Replace "../assets/github-logo.png" with the path to your GitHub logo image.
 import "../Component/Navbar.css";
 
 const MyNavbar = () => {
+  const [expanded, setExpanded] = useState(false);
+
   const scrollToTop = (event) => {
     event.preventDefault();
     window.scrollTo({ top: 0, behavior: "smooth" });
@@ -13,8 +15,13 @@ const MyNavbar = () => {
     event.preventDefault();
     const section = document.getElementById(sectionId);
     section.scrollIntoView({ behavior: "smooth" });
+
+    // Close the navbar on smaller devices after clicking a link
+    if (expanded) {
+      setExpanded(false);
+    }
   };
-  
+
   const renderTooltip = (props) => (
     <Tooltip id="github-tooltip" {...props}>
       GitHub
@@ -22,34 +29,65 @@ const MyNavbar = () => {
   );
 
   return (
-    <Navbar bg="light" expand="lg" className="fixed-top nav smooth-scroll">
+    <Navbar
+      bg="light"
+      expand="lg"
+      className="fixed-top nav smooth-scroll"
+      expanded={expanded}
+    >
       <Navbar.Brand className="custom-margin" href="/" onClick={scrollToTop}>
         Portfolio
       </Navbar.Brand>
-      <Navbar.Toggle aria-controls="navbarNavDropdown" />
+      <Navbar.Toggle
+        aria-controls="navbarNavDropdown"
+        onClick={() => setExpanded(!expanded)}
+      />
       <Navbar.Collapse id="navbarNavDropdown">
         <Nav className="mr-auto mt-2 mt-lg-0">
-          <Nav.Link href="about" onClick={(e) => scrollToSection(e, "about")} className="smooth-scroll navbar-ani-under">
+          <Nav.Link
+            href="about"
+            onClick={(e) => scrollToSection(e, "about")}
+            className="smooth-scroll navbar-ani-under"
+          >
             About
           </Nav.Link>
-          <Nav.Link href="academics" onClick={(e) => scrollToSection(e, "academics")} className="smooth-scroll navbar-ani-under">
+          <Nav.Link
+            href="academics"
+            onClick={(e) => scrollToSection(e, "academics")}
+            className="smooth-scroll navbar-ani-under"
+          >
             Academics
           </Nav.Link>
-          <Nav.Link href="personal" onClick={(e) => scrollToSection(e, "personal")} className="smooth-scroll navbar-ani-under">
+          <Nav.Link
+            href="personal"
+            onClick={(e) => scrollToSection(e, "personal")}
+            className="smooth-scroll navbar-ani-under"
+          >
             Personal
           </Nav.Link>
-          <Nav.Link href="contact" onClick={(e) => scrollToSection(e, "contact")} className="smooth-scroll navbar-ani-under">
+          <Nav.Link
+            href="contact"
+            onClick={(e) => scrollToSection(e, "contact")}
+            className="smooth-scroll navbar-ani-under"
+          >
             Contact me
           </Nav.Link>
         </Nav>
         {/* Add the GitHub logo button here */}
         <div className="github-logo-wrapper">
-        <OverlayTrigger placement="bottom" overlay={renderTooltip}>
-          <Nav.Link href="https://github.com/vtrivedi123/vishal-portfolio" target="_blank" rel="noopener noreferrer">
-            <img src={LogoImage} alt="GitHub Logo" className="github-logo" />
-          </Nav.Link>
-        </OverlayTrigger>
-      </div>
+          <OverlayTrigger
+            placement="bottom"
+            overlay={renderTooltip}
+          >
+            <Nav.Link
+              href="https://github.com/vtrivedi123/vishal-portfolio"
+              target="_blank"
+              rel="noopener noreferrer"
+            >
+              <img src={LogoImage} alt="GitHub Logo" className="github-logo" />
+            </Nav.Link>
+          </OverlayTrigger>
+        </div>
       </Navbar.Collapse>
     </Navbar>
   );
