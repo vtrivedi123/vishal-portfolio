1a04f678e85a3477d9ee0aa3ad8a0d1d20bd1456
Vishal Trivedi <vishal.trivedi5.2002@gmail.com>
2023-07-22 17:27:53 -0400

    Added Lorem ipsum placeholder for education tab

--- src/Pages/AboutMe.js
+++ src/Pages/AboutMe.js
@@ -10,10 +10,13 @@ const AboutMe = () => {
       content: (
         <div className='education-content'>
           <p>
-            I have X years of experience in web development...
-          </p>
-          <p>
-            Additional details...
+          Lorem ipsum dolor sit amet, consectetur adipiscing elit, 
+          sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. 
+          Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris 
+          nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in 
+          reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. 
+          Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia 
+          deserunt mollit anim id est laborum.
           </p>
         </div>
       ),
