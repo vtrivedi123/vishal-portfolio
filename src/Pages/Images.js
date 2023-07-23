dba68a5a0c31b930dda90b9df2eeb9e274e8e41b
Vishal Trivedi <vishal.trivedi5.2002@gmail.com>
2023-07-23 11:58:24 -0400

    Added logos

--- src/Pages/Images.js
+++ src/Pages/Images.js
@@ -1,15 +1,31 @@
 // images.js
 const images = [
   'Assets/html-icon.png',
-  'image2.jpg',
-  'image3.jpg',
+  'Assets/css-icon.png',
+  'Assets/java-icon.png',
+  'Assets/python-icon.png',
+  'Assets/CPP-icon.png',
+  'Assets/reactjs-icon.png',
+  'Assets/sw-icon.png',
+  'Assets/ps-icon.png',
+  'Assets/nodejs-icon.png',
+  'Assets/pr-icon.png',
+  'Assets/ms-icon.png',
   // Add more image URLs here
 ];
 
 const texts = [
   'HTML',
-  'Text for Image 2',
-  'Text for Image 3',
+  'CSS',
+  'Java',
+  'Python',
+  'C++',
+  'React JS',
+  'Solidworks',
+  'Adobe Photoshop',
+  'Node JS',
+  'Adobe Premiere Pro',
+  'Microsoft',
   // Add more text content here corresponding to each image
 ];
 
