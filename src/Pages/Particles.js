b98256b04165db52773563e0e534402a777c6b39
Vishal Trivedi <vishal.trivedi5.2002@gmail.com>
2023-07-25 18:24:35 -0400

    Added clouds

--- src/Pages/Particles.js
+++ src/Pages/Particles.js
@@ -3,19 +3,10 @@ import '../Component/ShootingStar.css';
 
 const ShootingStar = () => {
   return (
-    <div className="shooting-star-container">
-      <section>
-        <span></span>
-        <span></span>
-        <span></span>
-        <span></span>
-        <span></span>
-        <span></span>
-        <span></span>
-        <span></span>
-        <span></span>
-        <span></span>
-      </section>
+    <div className="relative w-full h-full">
+      <div className="stars fixed top-0 left-0 w-full h-full bg-repeat bg-top-center z-0"></div>
+      <div className="twinkling fixed top-0 left-0 w-full h-full bg-repeat bg-top-center z-0"></div>
+      <div className="clouds fixed top-0 left-0 w-full h-full bg-transparent bg-repeat bg-top-center opacity-40 z-2 animate-move-clouds-back"></div>
     </div>
   );
 };
--- tailwind.config.js
+++ tailwind.config.js
@@ -10,5 +10,10 @@ module.exports = {
       },
     },
   },
+  purge: [],
+  darkMode: false, // or 'media' or 'class'
+  variants: {
+    extend: {},
+  },
   plugins: [],
 };
