5fda9ad49383543d0f3fd89a1b7c6b6c05dc4dfd
Vishal Trivedi <vishal.trivedi5.2002@gmail.com>
2023-07-25 23:59:55 -0400

    Removed shooting stars

--- src/Pages/Particles.js
+++ src/Pages/Particles.js
@@ -5,18 +5,6 @@ const ShootingStar = () => {
   return (
     <div className="relative w-full h-full">
       <div className="shooting-star-container">
-      <section>
-        <span></span>
-        <span></span>
-        <span></span>
-        <span></span>
-        <span></span>
-        <span></span>
-        <span></span>
-        <span></span>
-        <span></span>
-        <span></span>
-      </section>
     </div>
       <div className="stars fixed top-0 left-0 w-full h-full bg-repeat bg-top-center z-0"></div>
       <div className="twinkling fixed top-0 left-0 w-full h-full bg-repeat bg-top-center z-0"></div>
