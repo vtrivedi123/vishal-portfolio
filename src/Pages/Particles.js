712f103c1c9855daee1ee032809a443d6eaf0448
Vishal Trivedi <vishal.trivedi5.2002@gmail.com>
2023-07-26 00:09:54 -0400

    Added star animation

--- src/Pages/Particles.js
+++ src/Pages/Particles.js
@@ -5,6 +5,11 @@ const ShootingStar = () => {
   return (
     <div className="relative w-full h-full">
       <div className="shooting-star-container">
+      <div id="stars-container">
+			<div id='stars'></div>
+			<div id='stars2'></div>
+			<div id='stars3'></div>
+    </div>
     </div>
       <div className="stars fixed top-0 left-0 w-full h-full bg-repeat bg-top-center z-0"></div>
       <div className="twinkling fixed top-0 left-0 w-full h-full bg-repeat bg-top-center z-0"></div>
